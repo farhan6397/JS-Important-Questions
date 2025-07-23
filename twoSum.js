b4fc518f5b32a6687c1b2b4fbdb30df7f6abc167
Farhan <151362771+farhan6397@users.noreply.github.com>
2025-07-23 15:06:57 +0530

    twoSum Optimized

--- twoSum.js
+++ twoSum.js
@@ -1,13 +1,30 @@
+// function twoSum(arr, target) {
+//     for(let i = 0; i < arr.length; i++) {
+//         for(let j = i + 1; j < arr.length; j++) {
+//             if(arr[i] + arr[j] === target) {
+//                 return [i,j];
+//             }
+//         }
+//     }
+// }
+
 function twoSum(arr, target) {
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = i + 1; j < arr.length; j++) {
-            if(arr[i] + arr[j] === target) {
-                return [i,j];
-            }
+    arr = arr.map((num, index) => ({ num, index }));
+    arr.sort((a, b) => a.num - b.num);
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        const sum = arr[left].num + arr[right].num;
+        if (sum === target) {
+            return [arr[left].index, arr[right].index];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
     }
 }
 
 let arr = [10, 5, 7, 6, 8, 9, 1];
-let target = 8;
+let target = 9;
 console.log(twoSum(arr, target)); // Output: [2, 6]
