a78824d2c7cf03b1a8dddd1643e07a371ea367b3
Farhan <151362771+farhan6397@users.noreply.github.com>
2025-07-24 23:24:19 +0530

    palindrome method 2

--- palindromeString.js
+++ palindromeString.js
@@ -1,25 +1,39 @@
-let s = "nitin";
-let isPalindrome = true;
+// method -1
+
+// let s = "nitin";
+// let isPalindrome = true;
 
 
-// for(let i=s.length-1; i>=0; i--){
-//     rev = rev + s.charAt(i);
+// // for(let i=s.length-1; i>=0; i--){
+// //     rev = rev + s.charAt(i);
+// // }
+
+// let first=0, last=s.length-1;
+// while(first<last){
+//     if(s.charAt(first) != s.charAt(last)){
+//        isPalindrome = false;
+//        break;
+//     }
+//     first++;
+//     last--;
 // }
 
-let first=0, last=s.length-1;
-while(first<last){
-    if(s.charAt(first) != s.charAt(last)){
-       isPalindrome = false;
-       break;
-    }
-    first++;
-    last--;
+// if(isPalindrome == true){
+//     console.log("palindrome");
+// }
+// else{
+//     console.log("not palindrome");
+    
+// }
+
+// method-2 
+// Easy way.
+
+let str = "naman";
+
+function isPalindrome(str){
+    rev = str.split("").reverse().join("");
+    return str === rev;
 }
 
-if(isPalindrome == true){
-    console.log("palindrome");
-}
-else{
-    console.log("not palindrome");
-    
-}
+console.log(isPalindrome(str));
