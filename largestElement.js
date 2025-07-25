c97318a8a30ea6178f9b60501ccc353cd977b7dd
Farhan <151362771+farhan6397@users.noreply.github.com>
2025-07-25 17:07:11 +0530

    largestElement method 2

--- largestElement.js
+++ largestElement.js
@@ -1,8 +1,28 @@
+// method-1
+// function largest(arr){
+//     arr.sort((a,b) => {
+//         return a-b;
+//     });
+//     return arr[arr.length-1];
+// }
+// let arr = [1,3,4,12,5,7,9]
+// console.log(largest(arr))
+
+// method-2
+
 function largest(arr){
-    arr.sort((a,b) => {
-        return a-b;
-    });
-    return arr[arr.length-1];
+    let l = 0;
+    let h = arr.length - 1;
+    let large = -Infinity;
+    
+    while (l <= h) {
+        large = Math.max(large, arr[l], arr[h]);
+        l++;
+        h--;
+    }
+    return large;
+
 }
-let arr = [1,3,4,12,5,7,9]
-console.log(largest(arr))
+
+let arr = [1, 3, 4, 2, 6, 8, 0];
+console.log(largest(arr)); // Output: 8
