44399401c73c6ecce0e72195fc7f8a6d7bab5717
Farhan <151362771+farhan6397@users.noreply.github.com>
2025-07-25 18:54:52 +0530

    secondMax version 2

--- secondMax.js
+++ secondMax.js
@@ -1,16 +1,14 @@
-let arr = [2, 4, 6, 7, 8, 9 ,0, 12, 12, 10]
+let arr = [2, 4, 6, 7, 8, 9 ,0, 12, 12, 10];
 
-let max = Math.max(arr[0], arr[1])
-let sMax = Math.min(arr[0], arr[1])
+let max = -Infinity;
+let sMax = -Infinity;
 
-for(let i=2; i<arr.length; i++){
-    if(max<arr[i]){
-        sMax=max;
-        max=arr[i]
-    }
-    else if(sMax<arr[i] && max!=arr[i]){
-        sMax=arr[i]
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+        sMax = max;
+        max = arr[i];
+    } else if (arr[i] > sMax && arr[i] < max) {
+        sMax = arr[i];
     }
 }
-console.log(sMax)
-
+console.log(sMax);
